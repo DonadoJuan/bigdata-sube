88db2a53d1e2579dc3c639e10cbb2e10ba41707d
Guido Spadavecchia <16108318+guidospadavecchia@users.noreply.github.com>
2019-09-17 20:49:14 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -6,11 +6,16 @@ def main():
     SOURCE_FILE = "data/data-source.csv"
     HDFS_OUTPUT_DIR = "hdfs://kudu:20500/Grupo1/procesado/"
 
+    print("Iniciando...")
     sh = SparkHelper()
+
     # Leemos el archivo CSV del origen
+    print("Leyendo el archivo " + SOURCE_FILE + " mediante Spark...")
     dataframe = sh.readcsv(SOURCE_FILE, True)
-    # Escribimos el dataframe en formato Parquet en el HDFS
+
+    # Escribimos el dataframe en los distintos formatos en el HDFS
     sh.writetoparquet(dataframe, HDFS_OUTPUT_DIR, "resultado.parquet")
+    print('Se escribió el archivo "resultado.parquet" en formato Parquet en "' + HDFS_OUTPUT_DIR + '"')
 
     return 0
 
