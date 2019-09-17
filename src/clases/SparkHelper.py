62bb91c0e4e657049bb6dd4153fe4b1bbf3bd2ed
Guido Spadavecchia <guido.spadavecchia@gmail.com>
2019-09-17 20:38:39 -0300

    Modificaciones de main y SparkHelper

--- src/clases/SparkHelper.py
+++ src/clases/SparkHelper.py
@@ -1,10 +1,15 @@
 from pyspark import SparkContext, SparkConf
 from pyspark.sql import SQLContext
 
-
 class SparkHelper:
 
     def readcsv(self, ruta, showheader=True):
+        """
+        Lee un archivo CSV con Spark y retorna un dataframe.
+        :param ruta: Ruta completa al archivo a leer.
+        :param showheader: Indica si se debe incluir el encabezado al leer el CSV.
+        :return: Objeto dataframe con los datos leídos.
+        """
         conf = SparkConf().set("spark.driver.memory", "10g") \
                           .set("spark.debug.maxToStringFields", "10000") \
                           .set("spark.driver.maxResultSize", "1g") \
@@ -14,15 +19,36 @@ class SparkHelper:
         sc = SparkContext(conf=conf)
         sp = SQLContext(sc)
         df = sp.read.csv(ruta, header=showheader)
+        df = self.renamecolumns(df)
         return df
 
-    def writetoparquet(self, dataframe, ruta, archivo):
-        rutacompleta = ruta + "/" + archivo
-        dataframe = self.renamecolumns(dataframe)
-        dataframe.write.parquet(rutacompleta)
-
     def renamecolumns(self, dataframe):
+        """
+        Renombra las columnas que vienen con caracteres inválidos en el header.
+        :param dataframe: Objeto dataframe que contiene los datos.
+        :return: Dataframe con las columnas renombradas.
+        """
         return dataframe.withColumnRenamed("Id Entidad", "IdEntidad") \
                         .withColumnRenamed("Id Ubicaci", "IdUbicacion") \
                         .withColumnRenamed("Tipo Ubica", "TipoUbicacion")
+
+    def writetoparquet(self, dataframe, ruta, archivo):
+        """
+        Escribe el dataframe pasado por parámetro en el HDFS en formato Parquet.
+        :param dataframe: Objeto dataframe a escribir.
+        :param ruta: Directorio en donde se guardará el archivo.
+        :param archivo: Nombre del archivo en formato Parquet.
+        """
+        rutacompleta = ruta + "/" + archivo
+        dataframe.write.format('parquet').mode("overwrite").save(rutacompleta)
+
+    def writetoavro(self, dataframe, ruta, archivo):
+        """
+        Escribe el dataframe pasado por parámetro en el HDFS en formato AVRO.
+        :param dataframe: Objeto dataframe a escribir.
+        :param ruta: Directorio en donde se guardará el archivo.
+        :param archivo: Nombre del archivo en formato AVRO.
+        """
+        rutacompleta = ruta + "/" + archivo
+        dataframe.write.format("com.databricks.spark.avro").mode('overwrite').save(rutacompleta)
 
