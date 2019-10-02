ca11053e8ed772f5bb2a410043b40eada388eff0
Guido Spadavecchia <guido.spadavecchia@gmail.com>
2019-10-01 21:14:58 -0300

    Desarrollo sobre SparkHelper y main

--- src/clases/SparkHelper.py
+++ src/clases/SparkHelper.py
@@ -1,7 +1,20 @@
+import os
+
 from pyspark import SparkContext, SparkConf
 from pyspark.sql import SQLContext
+from cassandra.cluster import Cluster
 
 class SparkHelper:
+
+    def __init__(self):
+        self.conf = SparkConf().set("spark.driver.memory", "10g") \
+                               .set("spark.debug.maxToStringFields", "10000") \
+                               .set("spark.driver.maxResultSize", "1g") \
+                               .setMaster("local[*]") \
+                               .setAppName("processCSV")
+        self.context = SparkContext(conf=self.conf)
+        self.context.setLogLevel('ERROR')
+        self.spark = SQLContext(self.context)
 
     def readcsv(self, ruta, showheader=True):
         """
@@ -10,17 +23,17 @@ class SparkHelper:
         :param showheader: Indica si se debe incluir el encabezado al leer el CSV.
         :return: Objeto dataframe con los datos leídos.
         """
-        conf = SparkConf().set("spark.driver.memory", "10g") \
-                          .set("spark.debug.maxToStringFields", "10000") \
-                          .set("spark.driver.maxResultSize", "1g") \
-                          .setMaster("local[*]") \
-                          .setAppName("processCSV")
-
-        sc = SparkContext(conf=conf)
-        sp = SQLContext(sc)
-        df = sp.read.csv(ruta, header=showheader)
+        df = self.spark.read.csv(ruta, header=showheader)
         df = self.renamecolumns(df)
         return df
+
+    def readparquet(self, rutahdfs):
+        """
+        Lee un archivo en formato Parquet del HDFS y retorna un Dataframe.
+        :param rutahdfs: Ruta del HDFS al archivo.
+        :return: Objeto dataframe que representa al archivo.
+        """
+        return self.spark.read.parquet(rutahdfs)
 
     def renamecolumns(self, dataframe):
         """
@@ -39,16 +52,22 @@ class SparkHelper:
         :param ruta: Directorio en donde se guardará el archivo.
         :param archivo: Nombre del archivo en formato Parquet.
         """
-        rutacompleta = ruta + "/" + archivo
+        rutacompleta = os.path.join(ruta, archivo)
         dataframe.write.format('parquet').mode("overwrite").save(rutacompleta)
 
-    def writetoavro(self, dataframe, ruta, archivo):
+    def writetocassandra(self, dataframe):
         """
-        Escribe el dataframe pasado por parámetro en el HDFS en formato AVRO.
+        Escribe el dataframe en la base de datos Cassandra.
         :param dataframe: Objeto dataframe a escribir.
-        :param ruta: Directorio en donde se guardará el archivo.
-        :param archivo: Nombre del archivo en formato AVRO.
         """
-        rutacompleta = ruta + "/" + archivo
-        dataframe.write.format("com.databricks.spark.avro").mode('overwrite').save(rutacompleta)
 
+
+    # def writetoavro(self, dataframe, ruta, archivo):
+    #     """
+    #     Escribe el dataframe pasado por parámetro en el HDFS en formato AVRO.
+    #     :param dataframe: Objeto dataframe a escribir.
+    #     :param ruta: Directorio en donde se guardará el archivo.
+    #     :param archivo: Nombre del archivo en formato AVRO.
+    #     """
+    #     rutacompleta = ruta + "/" + archivo
+    #     dataframe.write.format("com.databricks.spark.avro").mode('overwrite').save(rutacompleta)
--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+import os
+
 from src.clases.SparkHelper import SparkHelper
 
 
@@ -5,19 +7,29 @@ def main():
 
     SOURCE_FILE = "data/data-source.csv"
     HDFS_OUTPUT_DIR = "hdfs://kudu:20500/Grupo1/procesado/"
+    PARQUET_FILE = "resultado.parquet"
 
     print("Iniciando...")
     sh = SparkHelper()
 
     # Leemos el archivo CSV del origen
-    print("Leyendo el archivo " + SOURCE_FILE + " mediante Spark...")
+    print('Leyendo el archivo ' + SOURCE_FILE + ' mediante Spark...')
     dataframe = sh.readcsv(SOURCE_FILE, True)
 
     # Escribimos el dataframe en los distintos formatos en el HDFS
-    sh.writetoparquet(dataframe, HDFS_OUTPUT_DIR, "resultado.parquet")
+    sh.writetoparquet(dataframe, HDFS_OUTPUT_DIR, PARQUET_FILE)
     print('Se escribió el archivo "resultado.parquet" en formato Parquet en "' + HDFS_OUTPUT_DIR + '"')
+    print('===== Fin etapa 1 =====')
+    input('Presione ENTER para continuar...')
 
+    # Leemos el archivo en formato parquet del HDFS
+    print('Leyendo el archivo en formato Parquet del HDFS')
+    rutaarchivoParquet = os.path.join(HDFS_OUTPUT_DIR, PARQUET_FILE)
+    dataframeParquet = sh.readparquet(rutaarchivoParquet)
 
+    # Escribimos en Cassandra
+    print('Escribiendo el DataFrame en Cassandra...')
+    sh.writetocassandra(dataframeParquet)
 
     return 0
 
